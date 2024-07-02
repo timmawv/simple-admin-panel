079d39355a99a1f80a821e78970440e29b76ee6d
Timur <avlyakulovtimur@gmail.com>
2024-07-02 11:57:35 +0300

    add main-page

---
+++ src/main/java/avlyakulov/timur/filter/DatabaseFilter.java
@@ -0,0 +1,22 @@
+package avlyakulov.timur.filter;
+
+import avlyakulov.timur.config.db.FlywayInitializer;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebListener
+public class DatabaseFilter implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        FlywayInitializer flywayInitializer = new FlywayInitializer();
+        flywayInitializer.addMigrationsToDB();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ServletContextListener.super.contextDestroyed(sce);
+    }
+}
