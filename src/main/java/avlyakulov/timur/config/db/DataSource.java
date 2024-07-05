cec184b63221bb879c599477c8664208d04b61a0
Timur <avlyakulovtimur@gmail.com>
2024-07-05 15:10:38 +0300

    add DataSources with HikariPool and FlywayInitializer

---
+++ src/main/java/avlyakulov/timur/config/db/DataSource.java
@@ -0,0 +1,37 @@
+package avlyakulov.timur.config.db;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+@Slf4j
+public class DataSource {
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/users_db");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
+    private DataSource() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    public static void closeConnection() {
+        ds.close();
+    }
+}
