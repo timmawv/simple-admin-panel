42e3b2ba086debaf36764d131bcffc49bfe96cc7
Timur <avlyakulovtimur@gmail.com>
2024-07-05 15:10:58 +0300

    add FlywayListener

---
+++ src/main/java/avlyakulov/timur/listener/FlywayListener.java
@@ -0,0 +1,23 @@
+package avlyakulov.timur.listener;
+
+import avlyakulov.timur.config.db.DataSource;
+import avlyakulov.timur.config.db.FlywayInitializer;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebListener
+public class FlywayListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        FlywayInitializer flywayInitializer = new FlywayInitializer();
+        flywayInitializer.addMigrationsToDB();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        DataSource.closeConnection();
+    }
+}
