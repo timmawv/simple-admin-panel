d3978dc3133492b31f25eef610e3dfb8c273a735
Timur <avlyakulovtimur@gmail.com>
2024-07-05 15:44:33 +0300

    add logout

---
+++ src/main/java/avlyakulov/timur/servlet/LogoutServlet.java
@@ -0,0 +1,18 @@
+package avlyakulov.timur.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("user_login", null);
+        resp.sendRedirect("/simple_admin_panel/login");
+    }
+}
